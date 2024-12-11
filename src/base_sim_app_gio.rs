8d28ea7d80799eda3a7959127c8bfbf56ebb7008
Giovanni Pagliani <giovannipagliani@MBP-di-Giovanni.lan>
2024-12-11 16:04:02 +0100

    ops, last one i swear

--- src/base_sim_app_gio.rs
+++ src/base_sim_app_gio.rs
@@ -256,12 +256,14 @@ STRICTLY FOR SIM APP PART:
 2) make that the vectors in MyApp get filled from the informations on simcontroll.network_graph
 3) make the pop up bigger and such that it display the NodeEvent sent to the sim controll by that drone
 4) add simulation controller log in bottom panel.
+5) add connections between nodes
+
 
 --test everything, then continue with other things
 
-5) make functions add_drone and remove drone that not only eliminate graphically the drones and connections, but also in the network saved in sim controll
-6) add bottons in the pop ups for clients/servers that send flood req or certain messages
-7) at the end, change the circles in drones/clients/server small entities
+6) make functions add_drone and remove drone that not only eliminate graphically the drones and connections, but also in the network saved in sim controll
+7) add bottons in the pop ups for clients/servers that send flood req or certain messages
+8) at the end, change the circles in drones/clients/server small entities
 
 (.. more to come)
 
